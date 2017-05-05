b5df642a5b449f83b3495fd09680f9bbd18cb727
jahmul144 <erikhummel14@gmail.com>
2017-05-05 22:06:41 +0000

    updated urls to reflect new view funciton

--- djangobook/mysitetest/urls.py
+++ djangobook/mysitetest/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from views import current_datetime, hours_ahead
 
 urlpatterns = [
-	 url(r'^time/$',current_datetime),
+	 url(r'^time/$',current_datetime1),
 	 url(r'^time/plus/(\d{1,2})/$',hours_ahead),
 	 url(r'^admin/', admin.site.urls),
 ]
