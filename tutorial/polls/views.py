eea2ca8a2a634cf2ebfb3b6537e3f7b09ac77a00
jahmul144 <erikhummel14@gmail.com>
2017-05-28 16:51:25 +0000

    template render error polls/index1.html not polls.index1.html

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -10,5 +10,5 @@ def index(request):
 
 def questions(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	return render_to_response('polls.index1.html', {'question_latest': latest_question_list})
+	return render_to_response('polls/index1.html', {'question_latest': latest_question_list})
 
